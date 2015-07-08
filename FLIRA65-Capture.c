eff03933a6dbbb4aa8c54832ba50543ec0311357
Sam Moore <matches@ucc.asn.au>
2015-07-08 04:22:43 +0000

    Simplify FLIRA65-Capture
    Use arv_camera not arv_device
    
    Trying to get it to work even with a shitty stream...

--- FLIRA65-Capture.c
+++ FLIRA65-Capture.c
@@ -18,7 +18,7 @@ static int arv_option_height = -1;
 static int arv_option_horizontal_binning = -1;
 static int arv_option_vertical_binning = -1;
 static int arv_option_max_frames = 1;
-static int arv_option_max_errors_before_abort = 1000000000;
+static int arv_option_max_errors_before_abort = 10;
 static char * arv_option_save_prefix = "latest";
 static char * arv_option_save_type = "raw";
 
@@ -53,18 +53,13 @@ static const GOptionEntry arv_option_entries[] =
 	{ "frames",	'f',0, G_OPTION_ARG_INT, &arv_option_max_frames, "Number of frames to save", NULL},
 	{ "prefix", 'p',0, G_OPTION_ARG_STRING, &arv_option_save_prefix, "Prefix for frame files", NULL},
 	{ "type", 't',0, G_OPTION_ARG_STRING, &arv_option_save_type, "Type of file to save frames as", NULL},
+	{ "errors", 'e', 0, G_OPTION_ARG_INT, &arv_option_max_errors_before_abort, "Errors before aborting", NULL},
 	{ NULL }
 };
 
-typedef enum {
-	ARV_CAMERA_TYPE_BASLER,
-	ARV_CAMERA_TYPE_PROSILICA
-} ArvCameraType;
-
-int
-main (int argc, char **argv)
+int main (int argc, char **argv)
 {
-	ArvDevice *device;
+	ArvCamera * camera;
 	ArvStream *stream;
 	ArvBuffer *buffer;
 	GOptionContext *context;
@@ -87,6 +82,9 @@ main (int argc, char **argv)
 
 	g_option_context_free (context);
 	
+	if (arv_option_max_frames < 0)
+		arv_option_max_errors_before_abort = -1;
+	
 	save_buffer_fn = GetSaveBufferFn(arv_option_save_type);
 
 	arv_debug_enable (arv_option_debug_domains);
@@ -96,179 +94,84 @@ main (int argc, char **argv)
 	else
 		g_print ("Looking for camera '%s'\n", arv_option_camera_name);
 
-	device = arv_open_device (arv_option_camera_name);
+	camera = arv_camera_new (arv_option_camera_name);
 	
 	int errors = 0;
-	if (device != NULL) {
-		ArvGc *genicam;
-		ArvGcNode *node;
-		guint32 value;
-		guint32 maximum;
-		guint32 minimum;
-		guint64 n_processed_buffers;
-		guint64 n_failures;
-		guint64 n_underruns;
-		double v_double;
-		double v_double_min;
-		double v_double_max;
-		const char *v_string;
-		gboolean v_boolean;
+	if (camera == NULL)
+	{
+		g_print("No device found");
+		return 1;
+	}
 
-		genicam = arv_device_get_genicam (device);
-		#if 0
+	guint payload_size = arv_camera_get_payload(camera);
+	g_print ("payload size  = %d (0x%x)\n", payload_size, payload_size);
+	
 
-		if (arv_option_width > 0) {
-			node = arv_gc_get_node (genicam, "Width");
-			arv_gc_integer_set_value (ARV_GC_INTEGER (node), arv_option_width, NULL);
-		}
-		if (arv_option_height > 0) {
-			node = arv_gc_get_node (genicam, "Height");
-			arv_gc_integer_set_value (ARV_GC_INTEGER (node), arv_option_height, NULL);
-		}
-		if (arv_option_horizontal_binning > 0) {
-			node = arv_gc_get_node (genicam, "BinningHorizontal");
-			arv_gc_integer_set_value (ARV_GC_INTEGER (node), arv_option_horizontal_binning, NULL);
-		}
-		if (arv_option_vertical_binning > 0) {
-			node = arv_gc_get_node (genicam, "BinningVertical");
-			arv_gc_integer_set_value (ARV_GC_INTEGER (node), arv_option_vertical_binning, NULL);
-		}
+	stream = arv_camera_create_stream (camera, NULL, NULL);
+	if (arv_option_auto_buffer)
+	{
+		g_object_set (stream,"socket-buffer", ARV_GV_STREAM_SOCKET_BUFFER_AUTO,"socket-buffer-size", 0,NULL);
+	}
+	
+	for (i = 0; i < 30; i++)
+	{
+		arv_stream_push_buffer (stream, arv_buffer_new (payload_size, NULL));
+	}
 
-		node = arv_gc_get_node (genicam, "DeviceVendorName");
-		v_string = arv_gc_string_get_value (ARV_GC_STRING (node), NULL);
-		g_print ("vendor        = %s\n", v_string);
-		node = arv_gc_get_node (genicam, "DeviceModelName");
-		v_string = arv_gc_string_get_value (ARV_GC_STRING (node), NULL);
-		g_print ("model         = %s\n", v_string);
-		node = arv_gc_get_node (genicam, "DeviceID");
-		v_string = arv_gc_string_get_value (ARV_GC_STRING (node), NULL);
-		g_print ("device id     = %s\n", v_string);
-		node = arv_gc_get_node (genicam, "SensorWidth");
-		value = arv_gc_integer_get_value (ARV_GC_INTEGER (node), NULL);
-		g_print ("sensor width  = %d\n", value);
-		node = arv_gc_get_node (genicam, "SensorHeight");
-		value = arv_gc_integer_get_value (ARV_GC_INTEGER (node), NULL);
-		g_print ("sensor height = %d\n", value);
-		node = arv_gc_get_node (genicam, "Width");
-		value = arv_gc_integer_get_value (ARV_GC_INTEGER (node), NULL);
-		maximum = arv_gc_integer_get_max (ARV_GC_INTEGER (node), NULL);
-		g_print ("image width   = %d (max:%d)\n", value, maximum);
-		node = arv_gc_get_node (genicam, "Height");
-		value = arv_gc_integer_get_value (ARV_GC_INTEGER (node), NULL);
-		maximum = arv_gc_integer_get_max (ARV_GC_INTEGER (node), NULL);
-		g_print ("image height  = %d (max:%d)\n", value, maximum);
-		node = arv_gc_get_node (genicam, "BinningHorizontal");
-		value = arv_gc_integer_get_value (ARV_GC_INTEGER (node), NULL);
-		maximum = arv_gc_integer_get_max (ARV_GC_INTEGER (node), NULL);
-		minimum = arv_gc_integer_get_min (ARV_GC_INTEGER (node), NULL);
-		g_print ("horizontal binning  = %d (min:%d - max:%d)\n", value, minimum, maximum);
-		node = arv_gc_get_node (genicam, "BinningVertical");
-		value = arv_gc_integer_get_value (ARV_GC_INTEGER (node), NULL);
-		maximum = arv_gc_integer_get_max (ARV_GC_INTEGER (node), NULL);
-		minimum = arv_gc_integer_get_min (ARV_GC_INTEGER (node), NULL);
-		g_print ("vertical binning    = %d (min:%d - max:%d)\n", value, minimum, maximum);
-		node = arv_gc_get_node (genicam, "ExposureTimeAbs");
-		v_double = arv_gc_float_get_value (ARV_GC_FLOAT (node), NULL);
-		v_double_min = arv_gc_float_get_min (ARV_GC_FLOAT (node), NULL);
-		v_double_max = arv_gc_float_get_max (ARV_GC_FLOAT (node), NULL);
-		g_print ("exposure            = %g (min:%g - max:%g)\n", v_double, v_double_min, v_double_max);
-		node = arv_gc_get_node (genicam, "ExposureAuto");
-		v_string = arv_gc_enumeration_get_string_value (ARV_GC_ENUMERATION (node), NULL);
-		g_print ("exposure auto mode  = %s\n", v_string);
-		node = arv_gc_get_node (genicam, "GainRaw");
-		value = arv_gc_integer_get_value (ARV_GC_INTEGER (node), NULL);
-		maximum = arv_gc_integer_get_max (ARV_GC_INTEGER (node), NULL);
-		minimum = arv_gc_integer_get_min (ARV_GC_INTEGER (node), NULL);
-		g_print ("gain                = %d (min:%d - max:%d)\n", value, minimum, maximum);
-		node = arv_gc_get_node (genicam, "GainAuto");
-		v_string = arv_gc_enumeration_get_string_value (ARV_GC_ENUMERATION (node), NULL);
-		g_print ("gain auto mode      = %s\n", v_string);
-		node = arv_gc_get_node (genicam, "TriggerSelector");
-		v_string = arv_gc_enumeration_get_string_value (ARV_GC_ENUMERATION (node), NULL);
-		g_print ("trigger selector    = %s\n", v_string);
-		node = arv_gc_get_node (genicam, "ReverseX");
-		if (node != NULL) {
-			v_boolean = arv_gc_boolean_get_value (ARV_GC_BOOLEAN (node), NULL);
-			g_print ("reverse x          = %s\n", v_boolean ? "TRUE" : "FALSE");
-		}
-		#endif //0
 
-		stream = arv_device_create_stream (device, NULL, NULL);
-		if (arv_option_auto_buffer)
-			g_object_set (stream,
-				      "socket-buffer", ARV_GV_STREAM_SOCKET_BUFFER_AUTO,
-				      "socket-buffer-size", 0,
-				      NULL);
+	signal (SIGINT, set_cancel);
+	signal (SIGQUIT, set_cancel);
 
-		node = arv_gc_get_node (genicam, "PayloadSize");
-		value = arv_gc_integer_get_value (ARV_GC_INTEGER (node), NULL);
-		g_print ("payload size  = %d (0x%x)\n", value, value);
-
-		for (i = 0; i < 30; i++)
-			arv_stream_push_buffer (stream, arv_buffer_new (value, NULL));
-
-		arv_device_read_register (device, ARV_GVBS_STREAM_CHANNEL_0_PORT_OFFSET, &value, NULL);
-		g_print ("stream port = %d (%d)\n", value, arv_gv_stream_get_port (ARV_GV_STREAM (stream)));
-
-		arv_device_read_memory (device, 0x00014150, 8, memory_buffer, NULL);
-		arv_device_read_memory (device, 0x000000e8, 16, memory_buffer, NULL);
-		arv_device_read_memory (device,
-					ARV_GVBS_USER_DEFINED_NAME_OFFSET,
-					ARV_GVBS_USER_DEFINED_NAME_SIZE, memory_buffer, NULL);
-
-		node = arv_gc_get_node (genicam, "AcquisitionStart");
-		arv_gc_command_execute (ARV_GC_COMMAND (node), NULL);
-
-		signal (SIGINT, set_cancel);
-
-		int captured_frames = 0;
-		#define _CAN_STOP (arv_option_max_frames > 0 && captured_frames >= arv_option_max_frames)
-		do {
-			g_usleep (100000);
-
+	int captured_frames = 0;
+	guint64 timeout=1000000;
+	#define _CAN_STOP (arv_option_max_frames > 0 && captured_frames >= arv_option_max_frames)
+	do {
+		arv_camera_start_acquisition(camera);
+		g_usleep (100000);
 			do  {
-				buffer = arv_stream_pop_buffer (stream);
-				if (buffer == NULL) break;
-				ArvBufferStatus status = arv_buffer_get_status(buffer);
-				fprintf(stderr, "Status is %d", status);
-				if (status == ARV_BUFFER_STATUS_SUCCESS)
+			buffer = arv_stream_timeout_pop_buffer (stream, timeout);
+			if (buffer == NULL) break;
+			ArvBufferStatus status = arv_buffer_get_status(buffer);
+			fprintf(stderr, "Status is %d\n", status);
+			if (status == ARV_BUFFER_STATUS_SUCCESS)
+			{
+				if (timeout > 100000) timeout -= 1000;
+				errors = 0;
+				if (save_buffer_fn != NULL)
 				{
-					errors = 0;
-					if (save_buffer_fn != NULL)
-					{
-						char filename[BUFSIZ];
-						sprintf(filename, "%s%d.%s", arv_option_save_prefix, captured_frames, arv_option_save_type);
-						(*save_buffer_fn)(buffer, filename);
-						g_print("Saved frame %d to %s\n", captured_frames, filename);
-					}
-					captured_frames++;
+					char filename[BUFSIZ];
+					sprintf(filename, "%s%d.%s", arv_option_save_prefix, captured_frames, arv_option_save_type);
+					(*save_buffer_fn)(buffer, filename);
+					g_print("Saved frame %d to %s\n", captured_frames, filename);
 				}
-				else if (++errors > arv_option_max_errors_before_abort)
+				captured_frames++;
+			}
+			else 
+			{
+				if (timeout < 10000000) timeout+=1000;
+				fprintf(stderr, "%d errors out of %d allowed\n", errors, arv_option_max_errors_before_abort);
+				if (++errors > arv_option_max_errors_before_abort && arv_option_max_errors_before_abort >= 0)
 				{
 					set_cancel(SIGQUIT);
 				}
-				arv_stream_push_buffer (stream, buffer);
-			} while (!cancel && buffer != NULL && !_CAN_STOP);
-		} while (!cancel && !_CAN_STOP);
-		#undef _CAN_STOP
+			}
+			arv_stream_push_buffer (stream, buffer);
+			
+			arv_camera_stop_acquisition(camera);
+			
+		} while (!cancel && buffer != NULL && !_CAN_STOP);
+	} while (!cancel && !_CAN_STOP);
 
-		arv_device_read_register (device, ARV_GVBS_STREAM_CHANNEL_0_PORT_OFFSET, &value, NULL);
-		g_print ("stream port = %d (%d)\n", value, arv_gv_stream_get_port (ARV_GV_STREAM (stream)));
+	guint64 n_processed_buffers; guint64 n_failures; guint64 n_underruns;
+	arv_stream_get_statistics (stream, &n_processed_buffers, &n_failures, &n_underruns);
+	g_print ("Processed buffers = %Lu\n", (unsigned long long) n_processed_buffers);
+	g_print ("Failures          = %Lu\n", (unsigned long long) n_failures);
+	g_print ("Underruns         = %Lu\n", (unsigned long long) n_underruns);
 
-		arv_stream_get_statistics (stream, &n_processed_buffers, &n_failures, &n_underruns);
 
-		g_print ("Processed buffers = %Lu\n", (unsigned long long) n_processed_buffers);
-		g_print ("Failures          = %Lu\n", (unsigned long long) n_failures);
-		g_print ("Underruns         = %Lu\n", (unsigned long long) n_underruns);
-
-		node = arv_gc_get_node (genicam, "AcquisitionStop");
-		arv_gc_command_execute (ARV_GC_COMMAND (node), NULL);
-
-		g_object_unref (stream);
-		g_object_unref (device);
-	} else
-		g_print ("No device found\n");
-
+	g_object_unref (stream);
+	g_object_unref (camera);
+	
 	
 	return (errors > 0);
 }
