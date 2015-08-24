72a0ca21698f8cb538a951c0554d0b33fe68d346
Sam Moore <matches@ucc.asn.au>
2015-08-24 16:06:41 +0800

    normalise.py

--- normalise.py
+++ normalise.py
@@ -2,8 +2,3 @@
 from skimage.io import imread, imsave
 import numpy as np
 data = imread("latest0.png")
-mx = data.max()
-for x in range(data.shape[0]):
-	for y in range(data.shape[1]):
-		data[x,y] = int((data[x,y]/(1.*mx))*(2**16-1))
-imsave("normalised.png", data)
